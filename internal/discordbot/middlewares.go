b7c877d5f632487e5f79acdf1762452d163c5161
Khang Hoang <hoanghongkhang@gmail.com>
2022-07-07 02:58:26 +0700

    fix restrict roles middleware

--- internal/discordbot/middlewares.go
+++ internal/discordbot/middlewares.go
@@ -4,15 +4,25 @@ import (
 	"github.com/lus/dgc"
 )
 
+func isRoleAllowed(requireRoles []string, userRoles []string) bool {
+	if len(requireRoles) == 0 {
+		return true
+	}
+	for _, roleId := range userRoles {
+		for _, requiredRoleId := range requireRoles {
+			if roleId == requiredRoleId {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func restrictRolesMiddleware(next dgc.ExecutionHandler) dgc.ExecutionHandler {
 	return func(ctx *dgc.Ctx) {
-		for _, allowedRoleId := range ctx.Command.Flags {
-			for _, roleId := range ctx.Event.Member.Roles {
-				if roleId == allowedRoleId {
-					next(ctx)
-					return
-				}
-			}
+		if isRoleAllowed(ctx.Command.Flags, ctx.Event.Member.Roles) {
+			next(ctx)
+			return
 		}
 		ctx.RespondText("You do not have permission to perform this action.")
 	}
